5844fa146ab8bfc80427b80a76541bd80fbdf277
BootVirtual <m@ghencea.net>
2026-07-13 11:25:03 +0300

    Initial Commit

---
+++ CFR Platforms/CFR_PlatformsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CFR_PlatformsApp.swift
+//  CFR Platforms
+//
+//  Created by Marc-Tudor Ghencea on 13.07.26.
+//
+
+import SwiftUI
+
+@main
+struct CFR_PlatformsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CFR PlatformsTests/CFR_PlatformsTests.swift
@@ -0,0 +1,19 @@
+//
+//  CFR_PlatformsTests.swift
+//  CFR PlatformsTests
+//
+//  Created by Marc-Tudor Ghencea on 13.07.26.
+//
+
+import Testing
+@testable import CFR_Platforms
+
+struct CFR_PlatformsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
